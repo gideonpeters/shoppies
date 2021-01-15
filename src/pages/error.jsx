b8e4e4dfa06aa2420449dba58dfd6fd87b0580cc
Gideon Peters <my@pers.on.al>
2021-01-15 21:27:09 +0100

    chore: error page assets and color corrections

--- src/pages/error.jsx
+++ src/pages/error.jsx
@@ -10,7 +10,7 @@ const Error = () => {
 
     return <div className="h-screen flex flex-col items-center justify-center">
         <img src={ErrorImage} alt="not found" />
-        <button onClick={handleClick} className="mt-10 text-2xl bg-blue-400 text-white px-4 py-4 rounded hover:opacity-50">Go Back Home</button>
+        <button onClick={handleClick} className="mt-10 text-xl bg-green-400 text-white px-4 py-3 rounded hover:opacity-50">Go Back Home</button>
     </div>
 }
 
